f2ec1deb097f6967eb39a133b04b2e1ebce3a09a
Andrew Zigler <andrewzigler@gmail.com>
2019-09-15 20:09:15 -0700

    Remove default component
    - add native WebSocket support to Vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
+import VueNativeSock from 'vue-native-websocket'
+import store from './store'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+Vue.use(
+  VueNativeSock,
+  `ws://${process.env.VUE_APP_WEBSOCKET_URL}:${process.env.VUE_APP_WEBSOCKET_PORT}`,
+  {
+    format: 'json',
+    reconnection: true,
+    reconnectionAttempts: 1 * 100,
+    reconnectionDelay: 3 * 1000,
+    store
+  }
+)
 
 new Vue({
+  store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
